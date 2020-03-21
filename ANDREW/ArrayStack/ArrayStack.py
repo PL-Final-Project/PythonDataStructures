137ff97702afb8e65f8468783485e5d4ab5374ed
Andrew <andrew.gonzalez4@upr.edu>
2020-03-21 12:24:08 -0400

    Commented the ArrayStack DS.

--- ANDREW/ArrayStack/ArrayStack.py
+++ ANDREW/ArrayStack/ArrayStack.py
@@ -1,15 +1,18 @@
+#Definition of the ArrayStack Data Structure.
+#Coded by Andrew Gonzalez (github: andrewgonzalez4) using Python3 and Vim as text editor.
 class Stack:
+    #Initialization of the DS.
     def __init__(self):
         self.items = []
-
+    #Returns if the stack is empty.
     def isEmpty(self):
         return self.items == []
-
+    #Adds an item on top of the stack.
     def push(self, item):
         self.items.insert(0,item)
-
+    #Removes an item from the top of the stack.
     def pop(self):
         return self.items.pop(0)
-
+    #Prints the elements in the stack.
     def printStack(self):
         print(self.items)
